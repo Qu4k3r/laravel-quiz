fb040779545256de765efb73fe04c6f98220e176
l-neves <lucas.neves@agilize.com.br>
2024-04-16 00:22:34 -0300

    CONFIG - configura banco de dados para ambiente de teste | muda migrations para rodar em ambientes de teste | FEAT/REGISTER-USER executa primeiro teste de registro de usuario

--- src/database/migrations/0001_01_01_000005_create_quizzes_snapshots_triggers.php
+++ src/database/migrations/0001_01_01_000005_create_quizzes_snapshots_triggers.php
@@ -2,16 +2,35 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        if (App::environment('testing')) {
+            DB::unprepared('
+            CREATE TRIGGER trigger_no_updates
+            BEFORE UPDATE ON quizzes_snapshots
+            BEGIN
+                SELECT RAISE(FAIL, "Não é permitido fazer mudanças nesta tabela");
+            END;
+        ');
+
+            DB::unprepared('
+            CREATE TRIGGER trigger_no_delete
+            BEFORE DELETE ON quizzes_snapshots
+            BEGIN
+                SELECT RAISE(FAIL, "Não é permitido fazer mudanças nesta tabela");
+            END;
+        ');
+            return;
+        }
+
         DB::unprepared("
             CREATE OR REPLACE FUNCTION no_updates() RETURNS TRIGGER AS $$
             BEGIN
@@ -33,6 +52,13 @@ return new class extends Migration
      */
     public function down(): void
     {
+        if (App::environment('testing')) {
+            DB::unprepared('DROP TRIGGER IF EXISTS trigger_no_updates');
+
+            DB::unprepared('DROP TRIGGER IF EXISTS trigger_no_delete');
+            return;
+        }
+
         DB::unprepared('DROP TRIGGER IF EXISTS trigger_no_updates ON quizzes_snapshots');
         DB::unprepared('DROP FUNCTION IF EXISTS no_updates()');
     }
--- src/resources/js/Pages/Auth/Login.jsx
+++ src/resources/js/Pages/Auth/Login.jsx
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import Checkbox from '@/Components/Checkbox';
 import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
 import InputLabel from '@/Components/InputLabel';
 import PrimaryButton from '@/Components/PrimaryButton';
 import TextInput from '@/Components/TextInput';
-import { Head, Link, useForm } from '@inertiajs/react';
+import {Head, Link, useForm} from '@inertiajs/react';
 
-export default function Login({ status, canResetPassword }) {
-    const { data, setData, post, processing, errors, reset } = useForm({
+export default function Login({status, canResetPassword}) {
+    const {data, setData, post, processing, errors, reset} = useForm({
         email: '',
         password: '',
         remember: false,
@@ -28,13 +28,13 @@ export default function Login({ status, canResetPassword }) {
 
     return (
         <GuestLayout>
-            <Head title="Log in" />
+            <Head title="Log in"/>
 
             {status && <div className="mb-4 font-medium text-sm text-green-600">{status}</div>}
 
             <form onSubmit={submit}>
                 <div>
-                    <InputLabel htmlFor="email" value="Email" />
+                    <InputLabel htmlFor="email" value="Email"/>
 
                     <TextInput
                         id="email"
@@ -47,11 +47,11 @@ export default function Login({ status, canResetPassword }) {
                         onChange={(e) => setData('email', e.target.value)}
                     />
 
-                    <InputError message={errors.email} className="mt-2" />
+                    <InputError message={errors.email} className="mt-2"/>
                 </div>
 
                 <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                    <InputLabel htmlFor="password" value="Password"/>
 
                     <TextInput
                         id="password"
@@ -63,7 +63,7 @@ export default function Login({ status, canResetPassword }) {
                         onChange={(e) => setData('password', e.target.value)}
                     />
 
-                    <InputError message={errors.password} className="mt-2" />
+                    <InputError message={errors.password} className="mt-2"/>
                 </div>
 
                 <div className="block mt-4">
@@ -77,7 +77,7 @@ export default function Login({ status, canResetPassword }) {
                     </label>
                 </div>
 
-                <div className="flex items-center justify-end mt-4">
+                <div className="flex items-end justify-between mt-4">
                     {canResetPassword && (
                         <Link
                             href={route('password.request')}
@@ -86,9 +86,15 @@ export default function Login({ status, canResetPassword }) {
                             Forgot your password?
                         </Link>
                     )}
+                    <Link
+                        href={route('register')}
+                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Don't have an account? Sign up!
+                    </Link>
 
                     <PrimaryButton className="ms-4" disabled={processing}>
-                        Log in smf
+                        Log in
                     </PrimaryButton>
                 </div>
             </form>
